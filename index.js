8a9347ae958cbbd29788b9adf3c0993815c17666
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-14 04:19:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const currentUser= 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const currentUser = 'Edsger Dijkstra';
+ 
+...
+ 
+const shortGreeting = ... ;
+ 
+shortGreeting;
+//=> "Welcome, E!"
